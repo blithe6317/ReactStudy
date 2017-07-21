b4275877be22d2bb5b9ddc5747fa2326065babb0
admin <1160457037@qq.com>
2017-07-21 11:52:41 +0800

    首次测试

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+const element = <h1>Hello Word!</h1>
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
