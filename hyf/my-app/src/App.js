723a633cccb92957618b8b719aa5d2491e817697
丨Rainbow丨 <775183940@qq.com>
2017-07-25 13:42:34 +0800

    .

--- hyf/my-app/src/App.js
+++ hyf/my-app/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    render() {
+        return (
+            <div className="header">
+                <video class="top-back-video" id="topBackVideo" autoPlay="true" loop>
+                    <source src="//ossweb-img.qq.com/images/lol/v2/banner/back-v8.webm" type="video/webm"></source>
+                        <source src="//ossweb-img.qq.com/images/lol/v2/banner/back-v8.mp4" type="video/mp4"></source>
+                </video>
+                <div className="logo wobble"></div>
+            </div>
     );
-  }
-}
+    }
+    }
 
-export default App;
+    export default App;
