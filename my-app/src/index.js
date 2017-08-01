1a1262619d1ab49a9dc2f7c36203f506d62c0120
1160457037@qq.com <yu266317>
2017-08-01 23:20:03 +0800

    forms

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -330,6 +330,115 @@ const posts=[
 
 ReactDOM.render(<Blog posts={posts}/>,document.getElementById("blog"));
 
+class NameForm extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {value:''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({value:event.target.value});
+    }
+    handleSubmit(event){
+        alert("这个人的名字叫 ："+this.state.value);
+        event.preventDefault();
+    }
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        )
+    }
+}
+ReactDOM.render(<NameForm/>,document.getElementById("nameform"));
+
+class FlavorForm extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {value:"laoer"};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({value:event.target.value});
+    }
+    handleSubmit(event){
+        alert("你选择了"+this.state.value+"陪你睡！");
+        event.preventDefault();
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>请做出你的选择:</label>
+                <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="老大">老大</option>
+                    <option value="老二">老二</option>
+                    <option value="小三">小三</option>
+                    <option value="李俊">李俊</option>
+                    <option value="胡燃烽">胡燃烽</option>
+                </select>
+                <input type="submit" value="Submit"/>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(<FlavorForm/>,document.getElementById("flavorform"));
+
+class Reservation extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isGoing:true,
+            numberOfGuests:2
+        }
+        this.handleinputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.type==="checkbox"?target.checked:target.value;
+        const name = target.name;
+        this.setState({
+            [name]:value
+        });
+    }
+    render(){
+        return(
+            <form>
+                <label>
+                    那年de承诺：
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleinputChange}/>
+                </label>
+                <br/>
+                <label>
+                    无敌的老二：
+                    <input
+                    name="numberOfGuests"
+                    type="number"
+                    value={this.state.numberOfGuests}
+                    onChage={this.handleinputChange}
+                    />
+                </label>
+            </form>
+        )
+    }
+}
+ReactDOM.render(<Reservation/>,document.getElementById("reservation"));
+
+
+
 
 registerServiceWorker();
 
